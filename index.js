69b392879c83aea2cfe1aa3d8510d4156848c7c3
YadBro <ncada708@gmail.com>
2022-06-15 00:29:55 +0700

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000' //define client origin if both client and server have different origin
+        origin: 'https://dumbmerch-be-yadi.herokuapp.com/' //define client origin if both client and server have different origin
     }
 });
 require('./src/socket')(io);
